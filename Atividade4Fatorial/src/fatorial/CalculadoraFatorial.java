6674a5e8bd631e16110f99ef598257f91ef7f6a3
Vitor Campolina <103151856+vitorcampolina@users.noreply.github.com>
2025-05-16 17:47:18 -0300

    Alteração definitiva para verificar se é um número positivo.

--- Atividade4Fatorial/src/fatorial/CalculadoraFatorial.java
+++ Atividade4Fatorial/src/fatorial/CalculadoraFatorial.java
@@ -3,12 +3,14 @@ package fatorial;
 public class CalculadoraFatorial {
 
     public static int factorial(int n) {
+        if (n < 0) {
+            throw new IllegalArgumentException("Número deve ser positivo.");
+        }
 
         int result = 1;
         for (int i = 2; i <= n; i++) {
             result *= i;
         }
         return result;
-
     }
 }
